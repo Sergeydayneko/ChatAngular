16d52d255a81697d88d84b60bc99d0bb15b73e7d
Sergeydayneko <sergeydayneko@mail.ru>
2017-12-28 21:22:39 +0300

    + frontend: added materialize and start page;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MessageService } from './Messages/shared/messages.service'
+import { MessageService } from './CSApplication/shared/chatService/messages.service'
 import { AppComponent } from './app.component';
+import { LoginComponent } from './CSApplication/CSComponents/login/login.component';
+import {MaterializeModule} from 'angular2-materialize';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [
     MessageService
