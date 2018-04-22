2b19dd82c3e1b6f3ef76161aeb3f193771763e8d
Sergey Dayneko <sergeydayneko@mail.ru>
2018-04-22 16:18:22 +0300

    added libraries and component for general chat

--- src/app/CSApplication/CSComponents/chat/chat.component.ts
+++ src/app/CSApplication/CSComponents/chat/chat.component.ts
@@ -1,13 +1,37 @@
 import {Component} from "@angular/core";
-import {Product} from "../store/model/model.product";
+import * as Stomp from "stompjs";
+import * as SockJS from 'sockjs-client';
+import * as $ from 'jquery';
 
 @Component({
   templateUrl: "chat.component.html"
 })
 export class ChatComponent {
+  private serverUrl = 'http://localhost:8081/socket'
+  private title = 'WebSockets chat';
+  private stompClient;
 
-  products: Product[];
+  constructor(){
+    this.initializeWebSocketConnection();
+  }
 
-  constructor() {}
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/chat", (message) => {
+        if(message.body) {
+          $(".chat").append("<div class='message'>"+message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message){
+    this.stompClient.send("/app/send/message" , {}, message);
+    $('#input').val('');
+  }
 
 }
