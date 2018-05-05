297e8c468613f3c483dbdc835a1fcd75886d29b4
Sergey Dayneko <sergeydayneko@mail.ru>
2018-05-05 23:04:55 +0300

    + tuned chat component ;

--- src/app/CSApplication/CSComponents/chat/chat.component.ts
+++ src/app/CSApplication/CSComponents/chat/chat.component.ts
@@ -1,7 +1,8 @@
 import {Component} from "@angular/core";
 import * as Stomp from "stompjs";
 import * as SockJS from 'sockjs-client';
-import * as $ from 'jquery';
+import {CookieService} from "ngx-cookie-service";
+import {Message} from "./model/message";
 
 const PROTOCOL = "http";
 const PORT     = "8081";
@@ -13,9 +14,14 @@ const PORT     = "8081";
 export class ChatComponent {
   private chatUrl = '/socket';
   private stompClient;
+  public messages: Message[];
+  public username;
 
-  constructor(){
+  // TODO сделать сервис получения сохраненных сообщений из бэкэнда
+
+  constructor(private cookie: CookieService){
     this.initializeWebSocketConnection();
+    this.username = this.cookie.get("username");
   }
 
   initializeWebSocketConnection(){
@@ -25,88 +31,26 @@ export class ChatComponent {
     this.stompClient.connect({}, () => {
       that.stompClient.subscribe("/chat", (message) => {
         if(message.body) {
-          $(".chat").append("<div class='message'>"+message.body+"</div>")
-          console.log(message.body);
+          this.messages = [...this.messages, JSON.parse(message.body)]
         }
       });
     });
   }
 
-  sendMessage(message){
-    this.stompClient.send("/app/send/message" , {}, message);
+  sendMessage(text){
+    const data = {
+      username : this.username,
+      text     : text.trim()
+    };
 
+    this.stompClient.send("/app/send/message" , {}, JSON.stringify(data));
   }
-
-  //
-  // sendMessage(message) {
-  //   const nd = new Date;
-  //   const data = {
-  //     message,
-  //     userId: this.user,
-  //     time: nd.getHours() + ":" + nd.getMinutes()
-  //   };
-  //
-  //   this.messages = [...this.messages, data];
-  // }
-
-  messages = [];
-  totalOnline = 0;
-  user = 1;
 
   ngOnInit() {
     this.messages = [
-      {
-        message : 'Hi',
-        userId : 2,
-        time : "12:13"
-      },
-      {
-        message : 'Hi Dev',
-        userId : 1,
-        time : "12:13"
-      },
-      {
-        message : 'Whats up',
-        userId : 2,
-        time : "12:13"
-      },
-      {
-        message : 'Fine',
-        userId : 1,
-        time : "12:13"
-      },
-      {
-        message : 'All good? Whats new',
-        userId : 1,
-        time : "12:13"
-      },
-      {
-        message : 'Learning MEAN',
-        userId : 2,
-        time : "12:13"
-      },
-      {
-        message : 'Wow',
-        userId : 1,
-        time : "12:13"
-      },
-      {
-        message : 'This chat app is awesome',
-        userId : 2,
-        time : "12:13"
-      }
+      new Message("Serj", "hello everybody", "22:32:43"),
+      new Message("Roma", "hello everybody", "22:32:33"),
+      new Message("Igor", "hello everybody", "22:32:53")
     ]
   }
-
-  // sendMessage(message) {
-  //   const nd = new Date;
-  //   const data = {
-  //     message,
-  //     userId: this.user,
-  //     time: nd.getHours() + ":" + nd.getMinutes()
-  //   };
-  //
-  //   this.messages = [...this.messages, data];
-  // }
-
 }
--- src/app/CSApplication/CSComponents/chat/model/message.ts
+++ src/app/CSApplication/CSComponents/chat/model/message.ts
@@ -1,6 +1,7 @@
-/**
- * TODO необходимо поправить тип shptDate на дату, чтобы ложилась на дату из парсинга
- */
 export class Message{
-  constructor(public userId: number, public content: string, public shptDate: string) {}
+  constructor(
+    public username : string,
+    public text     : string,
+    public date     : string
+  ) {}
 }
