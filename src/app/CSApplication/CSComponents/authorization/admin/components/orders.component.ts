6bf958b8f0dbed64a0b77cfdb567f041499605f8
Sergey Dayneko <sergeydayneko@mail.ru>
2018-05-08 12:09:17 +0300

    + added templates and methods for admin module;

--- src/app/CSApplication/CSComponents/authorization/admin/components/orders.component.ts
+++ src/app/CSApplication/CSComponents/authorization/admin/components/orders.component.ts
@@ -1,8 +1,30 @@
 import {Component} from "@angular/core";
+import {ProductService} from "../../../store/service/product.service";
+import {Order} from "../../../store/model/order.model";
+import {OrderService} from "../../../store/service/order.service";
 
 @Component({
-  template: `<div>orders</div>`
+  templateUrl: "orders.component.html"
 })
 export class OrdersAdminComponent {
+  // Сделать кнопку для отображения всех заказов
+  includedShipped = false;
+
+  constructor(private orderRepository: OrderService) {}
+
+  getOrders(): Order[] {
+    return this.orderRepository.getOrders()
+      .filter(order => this.includedShipped || order.shipped)
+  }
+
+
+  markShipped(order: Order) {
+    order.shipped = true;
+    this.orderRepository.updateOrder(order);
+  }
+
+  delete(id: number) {
+    this.orderRepository.deleteOrder(id);
+  }
 
 }
--- src/app/CSApplication/CSComponents/authorization/admin/components/product-editor.component.ts
+++ src/app/CSApplication/CSComponents/authorization/admin/components/product-editor.component.ts
@@ -1,8 +1,37 @@
 import {Component} from "@angular/core";
+import {ProductService} from "../../../store/service/product.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Product} from "../../../store/model/product.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
-  template: `<div>Order editor</div>`
+  templateUrl: "product-editor.component.html"
 })
 export class ProductEditorComponent {
+  editing: boolean = false;
+  product: Product = new Product();
+
+  constructor(
+    private repository : ProductService,
+    private router     : Router,
+    activeRoute        : ActivatedRoute
+  ) {
+    this.editing = activeRoute.snapshot.params["mode"] == "edit";
+    if (this.editing) {
+      Object.assign(
+        this.product,
+        this.repository.getProduct(activeRoute.snapshot.params["id"])
+        );
+
+    }
+  }
+
+  // TODO для чего используется ngForm
+  save(form: NgForm) {
+    this.repository.saveProduct(this.product);
+    this.router.navigateByUrl("/admin/main/products");
+  }
+
+
 
 }
